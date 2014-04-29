355f2d3d4c37cb0de7dd2294dd5315da4ee28923
tomaszguzialek <tomasz.guzialek@humaninference.com>
2014-04-29 02:59:57 -0700

    Cassandra support. However, works extremely slowly - issue not diagnosed yet.

---
+++ HadoopDataCleaner/src/main/java/org/eobjects/hadoopdatacleaner/configuration/sample/SampleCassandraConfiguration.java
@@ -0,0 +1,137 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2013 Human Inference
+
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.hadoopdatacleaner.configuration.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eobjects.analyzer.beans.StringAnalyzer;
+import org.eobjects.analyzer.beans.filter.EqualsFilter;
+import org.eobjects.analyzer.beans.filter.ValidationCategory;
+import org.eobjects.analyzer.beans.transform.ConcatenatorTransformer;
+import org.eobjects.analyzer.beans.transform.TokenizerTransformer;
+import org.eobjects.analyzer.beans.valuedist.ValueDistributionAnalyzer;
+import org.eobjects.analyzer.beans.writers.InsertIntoTableAnalyzer;
+import org.eobjects.analyzer.configuration.AnalyzerBeansConfiguration;
+import org.eobjects.analyzer.configuration.AnalyzerBeansConfigurationImpl;
+import org.eobjects.analyzer.connection.Datastore;
+import org.eobjects.analyzer.connection.DatastoreCatalog;
+import org.eobjects.analyzer.connection.DatastoreCatalogImpl;
+import org.eobjects.analyzer.connection.PojoDatastore;
+import org.eobjects.analyzer.descriptors.Descriptors;
+import org.eobjects.analyzer.descriptors.SimpleDescriptorProvider;
+import org.eobjects.analyzer.job.AnalysisJob;
+import org.eobjects.analyzer.job.builder.AnalysisJobBuilder;
+import org.eobjects.analyzer.job.builder.AnalyzerJobBuilder;
+import org.eobjects.analyzer.job.builder.FilterJobBuilder;
+import org.eobjects.analyzer.job.builder.TransformerJobBuilder;
+import org.eobjects.metamodel.pojo.ArrayTableDataProvider;
+import org.eobjects.metamodel.pojo.TableDataProvider;
+import org.eobjects.metamodel.util.SimpleTableDef;
+
+public class SampleCassandraConfiguration {
+
+    public static AnalyzerBeansConfiguration buildAnalyzerBeansConfiguration() {
+        List<TableDataProvider<?>> tableDataProviders = new ArrayList<TableDataProvider<?>>();
+        SimpleTableDef tableDef1 = new SimpleTableDef("countrycodes", new String[] { "country_name", "iso2", "iso3" });
+        SimpleTableDef tableDef2 = new SimpleTableDef("countrycodes_output", new String[] { "country_name",
+                "iso2", "iso3" });
+        tableDataProviders.add(new ArrayTableDataProvider(tableDef1, new ArrayList<Object[]>()));
+        tableDataProviders.add(new ArrayTableDataProvider(tableDef2, new ArrayList<Object[]>()));
+        Datastore datastore = new PojoDatastore("countrycodes_cassandra", "countrycodes_schema", tableDataProviders);
+
+        DatastoreCatalog datastoreCatalog = new DatastoreCatalogImpl(datastore);
+
+        SimpleDescriptorProvider descriptorProvider = new SimpleDescriptorProvider(true);
+        descriptorProvider.addTransformerBeanDescriptor(Descriptors.ofTransformer(ConcatenatorTransformer.class));
+        descriptorProvider.addTransformerBeanDescriptor(Descriptors.ofTransformer(TokenizerTransformer.class));
+        descriptorProvider.addFilterBeanDescriptor(Descriptors.ofFilter(EqualsFilter.class));
+        descriptorProvider.addAnalyzerBeanDescriptor(Descriptors.ofAnalyzer(InsertIntoTableAnalyzer.class));
+        descriptorProvider.addAnalyzerBeanDescriptor(Descriptors.ofAnalyzer(StringAnalyzer.class));
+
+        return new AnalyzerBeansConfigurationImpl().replace(datastoreCatalog).replace(descriptorProvider);
+    }
+
+    public static AnalysisJob buildAnalysisJob(AnalyzerBeansConfiguration configuration) {
+        AnalysisJobBuilder ajb = new AnalysisJobBuilder(configuration);
+        try {
+            ajb.setDatastore("countrycodes_cassandra");
+
+            ajb.addSourceColumns("countrycodes_schema.countrycodes.country_name",
+                    "countrycodes_schema.countrycodes.iso2",
+                    "countrycodes_schema.countrycodes.iso3");
+
+            TransformerJobBuilder<ConcatenatorTransformer> concatenator = ajb
+                    .addTransformer(ConcatenatorTransformer.class);
+            concatenator.addInputColumns(ajb.getSourceColumnByName("iso2"));
+            concatenator.addInputColumns(ajb.getSourceColumnByName("iso3"));
+            concatenator.setConfiguredProperty("Separator", "_");
+            concatenator.getOutputColumns().get(0).setName("iso2_iso3");
+            
+            FilterJobBuilder<EqualsFilter, ?> equalsFilter = ajb.addFilter(EqualsFilter.class);
+            equalsFilter.addInputColumn(ajb.getSourceColumnByName("iso2"));
+            equalsFilter.setConfiguredProperty("Column", ajb.getSourceColumnByName("iso2"));
+            equalsFilter.setConfiguredProperty("Values", new String[] {"PL", "DK"});
+
+            // TransformerJobBuilder<TokenizerTransformer> tokenizer =
+            // ajb.addTransformer(TokenizerTransformer.class);
+            // tokenizer.setConfiguredProperty("Token target",
+            // TokenizerTransformer.TokenTarget.COLUMNS);
+            // tokenizer.addInputColumns(concatenator.getOutputColumns().get(0));
+            // tokenizer.setConfiguredProperty("Number of tokens", 2);
+            // tokenizer.setConfiguredProperty("Delimiters", new char[] { '_'
+            // });
+            // tokenizer.getOutputColumns().get(0).setName("tokenized");
+
+            AnalyzerJobBuilder<ValueDistributionAnalyzer> valueDistributionAnalyzer = ajb
+                    .addAnalyzer(ValueDistributionAnalyzer.class);
+            valueDistributionAnalyzer.setRequirement(equalsFilter.getOutcome(ValidationCategory.INVALID));
+            valueDistributionAnalyzer.addInputColumn(ajb.getSourceColumnByName("country_name"));
+
+            AnalyzerJobBuilder<ValueDistributionAnalyzer> valueDistributionAnalyzer2 = ajb
+                    .addAnalyzer(ValueDistributionAnalyzer.class);
+            valueDistributionAnalyzer2.setRequirement(equalsFilter.getOutcome(ValidationCategory.VALID));
+            valueDistributionAnalyzer2.addInputColumn(ajb.getSourceColumnByName("iso2"));
+
+            // AnalyzerJobBuilder<ValueDistributionAnalyzer>
+            // valueDistributionAnalyzer3 =
+            // ajb.addAnalyzer(ValueDistributionAnalyzer.class);
+            // valueDistributionAnalyzer3.addInputColumn(ajb.getSourceColumnByName("mainFamily:country_name"));
+            // valueDistributionAnalyzer3.addInputColumn(ajb.getSourceColumnByName("mainFamily:iso2"));
+
+            AnalyzerJobBuilder<StringAnalyzer> stringAnalyzer = ajb.addAnalyzer(StringAnalyzer.class);
+            stringAnalyzer.setRequirement(equalsFilter.getOutcome(ValidationCategory.VALID));
+            stringAnalyzer.addInputColumn(ajb.getSourceColumnByName("iso3"));
+
+            AnalyzerJobBuilder<InsertIntoTableAnalyzer> insertInto = ajb.addAnalyzer(InsertIntoTableAnalyzer.class);
+            insertInto.setConfiguredProperty("Column names", new String[] { "iso2" });
+            insertInto.setConfiguredProperty("Table name", "countrycodes_output");
+            insertInto.setConfiguredProperty("Schema name", "countrycodes_schema");
+            insertInto.setConfiguredProperty("Datastore",
+                    configuration.getDatastoreCatalog().getDatastore("countrycodes_cassandra"));
+            insertInto.setRequirement(equalsFilter.getOutcome(ValidationCategory.INVALID));
+            insertInto.addInputColumn(concatenator.getOutputColumns().get(0));
+
+            return ajb.toAnalysisJob();
+        } finally {
+            ajb.close();
+        }
+    }
+}
